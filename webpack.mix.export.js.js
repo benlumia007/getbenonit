c4ba513586ab6e668508ae833976c37e2acf6dd6
Benjamin Lu <benlumia007@users.noreply.github.com>
2021-07-15 13:35:23 -0700

    update file

--- webpack.mix.export.js.js
+++ webpack.mix.export.js.js
@@ -18,7 +18,7 @@ const rimraf  = require( 'rimraf' );
 const fs      = require( 'fs' );
 
 // Folder name to export the files to.
-let exportPath = 'benjlu';
+let exportPath = 'getbenonit';
 
 // Theme root-level files to include.
 let files = [
